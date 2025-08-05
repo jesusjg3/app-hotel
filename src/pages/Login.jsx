3a2d9aeade016c3783bdaa5926e466881b16c809
vanessaG0906 <vanessagomezpalma@gmail.com>
2025-08-05 12:28:18 -0500

    arreglando rutas x3

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,7 +7,7 @@ import { useAuthContext } from "../context/AuthContext";
 const API_BASE_URLS = [
   "http://localhost:8000/api",
   "http://127.0.0.1:8000/api",
-  "http://localhost:8080/api"
+  "http://localhost:8080/api",
   "https://steady-wallaby-inviting.ngrok-free.app/geshotel/api"
 ];
 
